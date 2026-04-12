1c8eff33995b970d9128e702f82c9279ca317e2d
Mahmud Hasan Shanto <sshanto12344321@gmail.com>
2026-04-12 19:12:31 +0600

    fix cors issue

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,23 @@ connectCloudinary()
 // ✅ MIDDLEWARES MUST COME BEFORE ROUTES
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+
+// app.use(cors())
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://docscheduler-appointment-management-68ha.onrender.com'
+  ],
+  methods: ['GET','POST','PUT','DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
+
+app.options('*', cors());
 
 // ✅ NOW register your API endpoints
 app.use('/api/admin', adminRouter)
